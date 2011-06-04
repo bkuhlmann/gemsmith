754a6c5004d94ca8c36bf6cc6d0db5ab1cd59a05
brooke <brooke@redalchemist.com>
2011-06-04 17:29:33 -0600

    Added all necessary templates.

--- lib/gemsmith/cli.rb
+++ lib/gemsmith/cli.rb
@@ -1,8 +1,16 @@
 require "yaml"
 require "thor"
+require "thor/actions"
 
 module Gemsmith
   class CLI < Thor
+    include Thor::Actions
+
+    # Overwritten Thor template source root.
+    def self.source_root
+      File.expand_path File.join(File.dirname(__FILE__), "templates")
+    end
+    
     # Initialize.
     def initialize args = [], options = {}, config = {}
       super
@@ -17,16 +25,46 @@ module Gemsmith
 
     desc "-c, [create=GEM_NAME]", "Create new gem."
     map "-c" => :create
+    method_option :bin, :aliases => "-b", :desc => "Add binary to gem build.", :type => :boolean, :default => false
     def create name
-      shell.say "Creating gem..."
-      options = {}
-      target = File.join Dir.pwd, name
-      template File.join("README.temp"), File.join(target, "README.rdoc"), options
-      template File.join("CHANGELOG.temp"), File.join(target, "CHANGELOG.rdoc"), options
-      template File.join("LICENSE.temp"), File.join(target, "LICENSE.rdoc"), options
-      shell.say "Gem created: #{name}"
+      shell.say "\nCreating gem..."
+
+      # Initialize options.
+      gem_name = name.downcase
+      gem_class = gem_name.capitalize
+      author_name = @settings[:author_name] || `git config user.name`.chomp || "TODO: Write full name here."
+      author_url = @settings[:author_url] || "TODO: Write home page URL here."
+      template_options = {
+        :gem_name => gem_name,
+        :gem_class => gem_class,
+        :gem_platform => (@settings[:gem_platform] || "Gem::Platform::RUBY"),
+        :author_name => author_name,
+        :author_email => (@settings[:author_email] || `git config user.email`.chomp || "TODO: Write email address here."),
+        :author_url => author_url,
+        :company_name => (@settings[:company_name] || author_name),
+        :company_url => (@settings[:company_url] || author_url),
+        :year => Time.now.year
+      }
+
+      # Apply templates.
+      target_path = File.join Dir.pwd, gem_name
+      empty_directory File.join(target_path, "lib", gem_name)
+      template "README.rdoc.tmp", File.join(target_path, "README.rdoc"), template_options
+      template "CHANGELOG.rdoc.tmp", File.join(target_path, "CHANGELOG.rdoc"), template_options
+      template "LICENSE.rdoc.tmp", File.join(target_path, "LICENSE.rdoc"), template_options
+      template "Gemfile.tmp", File.join(target_path, "Gemfile"), template_options
+      template "Rakefile.tmp", File.join(target_path, "Rakefile"), template_options
+      template "gitignore.tmp", File.join(target_path, ".gitignore"), template_options
+      template "gem.gemspec.tmp", File.join(target_path, "#{gem_name}.gemspec"), template_options
+      template File.join("lib", "gem.rb.tmp"), File.join(target_path, "lib", "#{gem_name}.rb"), template_options
+      template File.join("lib", "gem", "version.rb.tmp"), File.join(target_path, "lib", gem_name, "version.rb"), template_options
+      if options[:bin]
+        template File.join("bin", "gem.tmp"), File.join(target_path, "bin", gem_name), template_options
+      end
+      
+      shell.say "Gem created: #{gem_name}\n\n"
     end
-    
+
     desc "-v, [version]", "Show version."
     map "-v" => :version
     def version
@@ -38,7 +76,7 @@ module Gemsmith
       shell.say and super
     end
 
-    protected
+    private
     
     # Load settings.
     def load_settings
@@ -52,13 +90,6 @@ module Gemsmith
       end
     end
     
-    # Save settings.
-    # ==== Parameters
-    # * +settings+ - Required. Saves settings to file.
-    def save_settings settings
-      File.open(@settings_file, 'w') {|file| file << YAML::dump(settings)}
-    end
-
     # Print version information.
     def print_version
       shell.say "Gemsmith " + VERSION
