0f23c2c544698d98b2ab1adf06d7968d21d158ff
Brooke Kuhlmann <brooke@redalchemist.com>
2013-01-13 17:12:01 -0700

    Added spec focus capability. [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
 require "bundler/setup"
 require "gemsmith"
 require File.join "gemsmith", "cli"
+
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+end
