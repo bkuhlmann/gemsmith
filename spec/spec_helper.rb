c14525aed0f5a356e569567a578b5ce566995447
Brooke Kuhlmann <brooke@redalchemist.com>
2013-04-07 10:17:39 -0600

    Treat symbols and true values by default when running RSpec specs. [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require "gemsmith/cli"
 require "pry"
 
 RSpec.configure do |config|
-  config.filter_run focus: true
   config.run_all_when_everything_filtered = true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
 end
