db40fa75c35e05e1e2bd5981d44640207f2c5427
Brooke Kuhlmann <brooke@redalchemist.com>
2013-06-14 20:15:50 -0600

    Added the rspec_install_path helper method for the RSpec Skeleton class.

--- lib/gemsmith/skeletons/rspec_skeleton.rb
+++ lib/gemsmith/skeletons/rspec_skeleton.rb
@@ -3,8 +3,14 @@ module Gemsmith
     class RspecSkeleton < BaseSkeleton
       def create_files
         template "rspec.tmp", File.join(install_path, ".rspec"), template_options
-        template File.join("spec", "spec_helper.rb.tmp"), File.join(install_path, "spec", "spec_helper.rb"), template_options
-        template File.join("spec", "gem_spec.rb.tmp"), File.join(install_path, "spec", "#{gem_name}_spec.rb"), template_options
+        template File.join("spec", "spec_helper.rb.tmp"), File.join(rspec_install_path, "spec_helper.rb"), template_options
+        template File.join("spec", "gem_spec.rb.tmp"), File.join(rspec_install_path, "#{gem_name}_spec.rb"), template_options
+      end
+
+      private
+
+      def rspec_install_path
+        File.join install_path, "spec"
       end
     end
   end
