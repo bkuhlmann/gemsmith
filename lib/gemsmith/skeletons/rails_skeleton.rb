88d354263f9aa3ccf5314798d0fae672b179a065
Brooke Kuhlmann <brooke@redalchemist.com>
2013-06-14 20:20:40 -0600

    Added generators_source_path and generators_install_path helper methods to the Rails Skeleton class.

--- lib/gemsmith/skeletons/rails_skeleton.rb
+++ lib/gemsmith/skeletons/rails_skeleton.rb
@@ -16,17 +16,27 @@ module Gemsmith
       end
 
       def create_generator_files
-        empty_directory File.join(lib_install_path, "generators", gem_name, "templates")
-        template File.join("lib", "generators", "gem", "install", "install_generator.rb.tmp"), File.join(lib_install_path, "generators", gem_name, "install", "install_generator.rb"), template_options
-        template File.join("lib", "generators", "gem", "install", "USAGE.tmp"), File.join(lib_install_path, "generators", gem_name, "install", "USAGE"), template_options
-        template File.join("lib", "generators", "gem", "upgrade", "upgrade_generator.rb.tmp"), File.join(lib_install_path, "generators", gem_name, "upgrade", "upgrade_generator.rb"), template_options
-        template File.join("lib", "generators", "gem", "upgrade", "USAGE.tmp"), File.join(lib_install_path, "generators", gem_name, "upgrade", "USAGE"), template_options
+        empty_directory File.join(generators_install_path, "templates")
+        template File.join(generators_source_path, "install", "install_generator.rb.tmp"), File.join(generators_install_path, "install", "install_generator.rb"), template_options
+        template File.join(generators_source_path, "install", "USAGE.tmp"), File.join(generators_install_path, "install", "USAGE"), template_options
+        template File.join(generators_source_path, "upgrade", "upgrade_generator.rb.tmp"), File.join(generators_install_path, "upgrade", "upgrade_generator.rb"), template_options
+        template File.join(generators_source_path, "upgrade", "USAGE.tmp"), File.join(generators_install_path, "upgrade", "USAGE"), template_options
       end
 
       def create_travis_gemfiles
         if template_options[:travis]
           template File.join("gemfiles", "rails-3.2.x.gemfile.tmp"), File.join(install_path, "gemfiles", "rails-3.2.x.gemfile"), template_options
         end
+      end
+
+      private
+
+      def generators_source_path
+        File.join "lib", "generators", "gem"
+      end
+
+      def generators_install_path
+        File.join lib_install_path, "generators", gem_name
       end
     end
   end
