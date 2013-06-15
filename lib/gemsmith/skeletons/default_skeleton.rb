6509f1d3f20a45f36894993475b35efc0260943c
Brooke Kuhlmann <brooke@redalchemist.com>
2013-06-14 20:08:09 -0600

    Split out ruby and git files from default files.

--- lib/gemsmith/skeletons/default_skeleton.rb
+++ lib/gemsmith/skeletons/default_skeleton.rb
@@ -1,14 +1,20 @@
 module Gemsmith
   module Skeletons
     class DefaultSkeleton < BaseSkeleton
-      def create_files
+      def create_default_files
         template "Gemfile.tmp", File.join(install_path, "Gemfile"), template_options
         template "Rakefile.tmp", File.join(install_path, "Rakefile"), template_options
-        template "gitignore.tmp", File.join(install_path, ".gitignore"), template_options
-        template "ruby-version.tmp", File.join(install_path, ".ruby-version"), template_options
         template "gem.gemspec.tmp", File.join(install_path, "#{gem_name}.gemspec"), template_options
         template File.join("lib", "gem.rb.tmp"), File.join(install_path, "lib", "#{gem_name}.rb"), template_options
         template File.join("lib", "gem", "version.rb.tmp"), File.join(install_path, "lib", gem_name, "version.rb"), template_options
+      end
+
+      def create_ruby_files
+        template "ruby-version.tmp", File.join(install_path, ".ruby-version"), template_options
+      end
+
+      def create_git_files
+        template "gitignore.tmp", File.join(install_path, ".gitignore"), template_options
       end
     end
   end
