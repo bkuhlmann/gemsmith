c623fd8a9d2855d7f1a6c37758e758d5c040912a
Brooke Kuhlmann <brooke@redalchemist.com>
2013-12-27 15:47:22 -0700

    Added Pry plugin requirements to RSpec spec helper. [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,13 @@ require "bundler/setup"
 require "gemsmith"
 require "gemsmith/cli"
 require "pry"
+require "pry-byebug"
+require "pry-remote"
+require "pry-rescue"
+require "pry-stack_explorer"
+require "pry-vterm_aliases"
+require "pry-git"
+require "pry-doc"
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
