f3f8263394f14b37dd203fe8084d28f38ee0c365
Brooke Kuhlmann <brooke@redalchemist.com>
2013-12-28 10:12:24 -0700

    Fixed RSpec expect syntax for specs.

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -16,52 +16,52 @@ describe Gemsmith::CLI do
       cli.stub(:ruby_patch).and_return "p0"
 
       options = cli.send :initialize_template_options, "test"
-      options[:gem_name].should be == "test"
-      options[:gem_class].should be == "Test"
-      options[:gem_platform].should be == "Gem::Platform::RUBY"
-      options[:gem_url].should be == url
-      options[:author_name].should be == name
-      options[:author_email].should be == (Gemsmith::Kit.git_config_value("user.email") || "TODO: Add email address here.")
-      options[:author_url].should be == url
-      options[:company_name].should be == name
-      options[:company_url].should be == url
-      options[:github_user].should be == (Gemsmith::Kit.git_config_value("github.user") || "unknown")
-      options[:year].should be == Time.now.year
-      options[:ruby_version].should be == "2.0.0"
-      options[:ruby_patch].should be == "p0"
-      options[:rails_version].should be == "3.0"
-      options[:post_install_message].should be == nil
-      options[:bin].should be_false
-      options[:rails].should be_false
-      options[:pry].should be_true
-      options[:guard].should be_true
-      options[:rspec].should be_true
-      options[:travis].should be_true
-      options[:code_climate].should be_true
+      expect(options[:gem_name]).to eq("test")
+      expect(options[:gem_class]).to eq("Test")
+      expect(options[:gem_platform]).to eq("Gem::Platform::RUBY")
+      expect(options[:gem_url]).to eq(url)
+      expect(options[:author_name]).to eq(name)
+      expect(options[:author_email]).to eq((Gemsmith::Kit.git_config_value("user.email") || "TODO: Add email address here."))
+      expect(options[:author_url]).to eq(url)
+      expect(options[:company_name]).to eq(name)
+      expect(options[:company_url]).to eq(url)
+      expect(options[:github_user]).to eq((Gemsmith::Kit.git_config_value("github.user") || "unknown"))
+      expect(options[:year]).to eq(Time.now.year)
+      expect(options[:ruby_version]).to eq("2.0.0")
+      expect(options[:ruby_patch]).to eq("p0")
+      expect(options[:rails_version]).to eq("3.0")
+      expect(options[:post_install_message]).to eq(nil)
+      expect(options[:bin]).to eq(false)
+      expect(options[:rails]).to eq(false)
+      expect(options[:pry]).to eq(true)
+      expect(options[:guard]).to eq(true)
+      expect(options[:rspec]).to eq(true)
+      expect(options[:travis]).to eq(true)
+      expect(options[:code_climate]).to eq(true)
     end
 
     it "loads custom settings" do
       custom_settings = cli.send :load_yaml, File.join(File.dirname(__FILE__), "support", "settings.yml")
 
       options = cli.send :initialize_template_options, "test", custom_settings
-      options[:gem_name].should be == "test"
-      options[:gem_class].should be == "Test"
-      options[:gem_platform].should be == "Gem::Platform::CURRENT"
-      options[:author_name].should be == "Testy Tester"
-      options[:author_email].should be == "test@test.com"
-      options[:author_url].should be == "https://www.test.com"
-      options[:gem_url].should be == "https://www.gem.com"
-      options[:company_name].should be == "ACME"
-      options[:company_url].should be == "https://www.acme.com"
-      options[:github_user].should be == "tester"
-      options[:year].should be == 1920
-      options[:ruby_version].should be == "1.8.0"
-      options[:rails_version].should be == "2.3.0"
-      options[:post_install_message].should be == "Follow @tester on Twitter for more info."
-      options[:bin].should be_false
-      options[:rails].should be_false
-      options[:rspec].should be_true
-      options[:code_climate].should be_true
+      expect(options[:gem_name]).to eq("test")
+      expect(options[:gem_class]).to eq("Test")
+      expect(options[:gem_platform]).to eq("Gem::Platform::CURRENT")
+      expect(options[:author_name]).to eq("Testy Tester")
+      expect(options[:author_email]).to eq("test@test.com")
+      expect(options[:author_url]).to eq("https://www.test.com")
+      expect(options[:gem_url]).to eq("https://www.gem.com")
+      expect(options[:company_name]).to eq("ACME")
+      expect(options[:company_url]).to eq("https://www.acme.com")
+      expect(options[:github_user]).to eq("tester")
+      expect(options[:year]).to eq(1920)
+      expect(options[:ruby_version]).to eq("1.8.0")
+      expect(options[:rails_version]).to eq("2.3.0")
+      expect(options[:post_install_message]).to eq("Follow @tester on Twitter for more info.")
+      expect(options[:bin]).to eq(false)
+      expect(options[:rails]).to eq(false)
+      expect(options[:rspec]).to eq(true)
+      expect(options[:code_climate]).to eq(true)
     end
   end
 end
--- spec/skeletons/base_skeleton_spec.rb
+++ spec/skeletons/base_skeleton_spec.rb
@@ -19,23 +19,23 @@ describe Gemsmith::Skeletons::BaseSkeleton do
 
   describe "#respond_to?" do
     it "responds to template" do
-      skeleton.respond_to?("template").should be_true
+      expect(skeleton.respond_to?("template")).to eq(true)
     end
 
     it "responds to template_options" do
-      skeleton.respond_to?("template_options").should be_true
+      expect(skeleton.respond_to?("template_options")).to eq(true)
     end
 
     it "responds to gem_name" do
-      skeleton.respond_to?("gem_name").should be_true
+      expect(skeleton.respond_to?("gem_name")).to eq(true)
     end
 
     it "responds to destination_root" do
-      skeleton.respond_to?("destination_root").should be_true
+      expect(skeleton.respond_to?("destination_root")).to eq(true)
     end
 
     it "does not responds to a bogus method" do
-      skeleton.respond_to?("bogus_method").should be_false
+      expect(skeleton.respond_to?("bogus_method")).to eq(false)
     end
   end
 end
