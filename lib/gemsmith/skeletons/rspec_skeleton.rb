78599ecde9e8a3b78075975dfd5d7b9e998e93c1
Brooke Kuhlmann <brooke@alchemists.io>
2015-03-27 20:46:22 -0600

    Removed default RSpec configuration. [ci skip]

--- lib/gemsmith/skeletons/rspec_skeleton.rb
+++ lib/gemsmith/skeletons/rspec_skeleton.rb
@@ -2,7 +2,6 @@ module Gemsmith
   module Skeletons
     class RspecSkeleton < BaseSkeleton
       def create_files
-        template "%gem_name%/.rspec.tt", template_options
         template "#{rspec_root}/spec_helper.rb.tt", template_options
         template "#{rspec_root}/%gem_name%_spec.rb.tt", template_options
         template "#{rspec_root}/support/kit/default_config.rb.tt", template_options
