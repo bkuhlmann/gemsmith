dea76e52a7833b7fa874a28cf03b123027e82fcd
Brooke Kuhlmann <brooke@alchemists.io>
2015-07-22 20:45:32 -0600

    Added CODE OF CONDUCT to template install.
    - Ensures the CODE OF CONDUCT is installed when
      building new gem skeletons.
    
    [ci skip]

--- lib/gemsmith/skeletons/documentation_skeleton.rb
+++ lib/gemsmith/skeletons/documentation_skeleton.rb
@@ -4,6 +4,7 @@ module Gemsmith
       def create_files
         template "%gem_name%/README.md.tt", template_options
         template "%gem_name%/CONTRIBUTING.md.tt", template_options
+        template "%gem_name%/CODE_OF_CONDUCT.md.tt", template_options
         template "%gem_name%/LICENSE.md.tt", template_options
         template "%gem_name%/CHANGELOG.md.tt", template_options
       end
