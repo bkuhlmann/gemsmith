8a3e0ed512eaa3f56c1bcccf1fa55dfb1b6f2584
Brooke Kuhlmann <brooke@alchemists.io>
2015-08-30 16:35:45 -0600

    Updated TravisSkeleton to be optional.
    - Necessary to determine if it can create
      skeleton files or not.

--- lib/gemsmith/skeletons/travis_skeleton.rb
+++ lib/gemsmith/skeletons/travis_skeleton.rb
@@ -2,7 +2,12 @@ module Gemsmith
   module Skeletons
     # Configures Travis CI support.
     class TravisSkeleton < BaseSkeleton
+      def enabled?
+        cli.template_options.key?(:travis) && cli.template_options[:travis]
+      end
+
       def create
+        return unless enabled?
         cli.template "%gem_name%/.travis.yml.tt", cli.template_options
       end
     end
--- spec/lib/gemsmith/skeletons/travis_skeleton_spec.rb
+++ spec/lib/gemsmith/skeletons/travis_skeleton_spec.rb
@@ -8,11 +8,25 @@ describe Gemsmith::Skeletons::TravisSkeleton, :temp_dir do
   subject { described_class.new cli }
   before { FileUtils.mkdir gem_dir }
 
+  it_behaves_like "an optional skeleton", :travis
+
   describe "#create" do
     before { subject.create }
 
-    it "creates Travic CI configuration" do
-      expect(cli).to have_received(:template).with("%gem_name%/.travis.yml.tt", options)
+    context "when enabled" do
+      let(:options) { {travis: true} }
+
+      it "creates Travic CI configuration" do
+        expect(cli).to have_received(:template).with("%gem_name%/.travis.yml.tt", options)
+      end
+    end
+
+    context "when disabled" do
+      let(:options) { {travis: false} }
+
+      it "creates Travic CI configuration" do
+        expect(cli).to_not have_received(:template)
+      end
     end
   end
 end
