bfdd139fa1c22252d34811744b7eb18656fa93ff
Brooke Kuhlmann <brooke@alchemists.io>
2015-09-27 08:03:09 -0600

    Fixed RSpec temp dir cleanup.
    - Fixes issue with temp folder being left around after each test.

--- spec/support/kit/temp_dir.rb
+++ spec/support/kit/temp_dir.rb
@@ -14,9 +14,10 @@ RSpec.configure do |config|
   config.include RSpec::Kit::TempDirContext
 
   config.before do |example|
-    if example.metadata[:temp_dir]
-      FileUtils.rm_rf(temp_dir) if File.exist?(temp_dir)
-      FileUtils.mkdir_p(temp_dir)
-    end
+    FileUtils.mkdir_p(temp_dir) if example.metadata[:temp_dir]
+  end
+
+  config.after do |example|
+    FileUtils.rm_rf(temp_dir) if example.metadata[:temp_dir]
   end
 end
