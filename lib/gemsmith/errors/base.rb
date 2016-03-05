5a4b702df65922f3bf767354399ea7006368ab78
Brooke Kuhlmann <brooke@alchemists.io>
2016-03-05 14:31:34 -0700

    Added gem specific error classes.
    - `Gemsmith::Errors::Base` will be used to capture all gem
      related errors.
    - Specific errors will inherit from `Gemsmith::Errors::Base` in
      case downstream apps want to capture all or specific Gemsmith
      related errors.

---
+++ lib/gemsmith/errors/base.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Gemsmith
+  module Errors
+    # The base error class for all gem related errors.
+    class Base < StandardError
+    end
+  end
+end
---
+++ lib/gemsmith/errors/gem_specification.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Gemsmith
+  module Errors
+    # The error class for gem specification errors.
+    class GemSpecification < Base
+    end
+  end
+end
