969297eb93b78be17e6c92eb33dae9290286cb4a
Brooke Kuhlmann <brooke@alchemists.io>
2016-03-13 13:49:47 -0600

    Refactored Rake release object to use gem credentials.
    - With management of gem credentials fully supported, this object
      no longer needs to deal with this knowledge.

--- lib/gemsmith/rake/release.rb
+++ lib/gemsmith/rake/release.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "milestoner"
+require "gemsmith/credentials"
 
 module Gemsmith
   module Rake
@@ -11,22 +12,23 @@ module Gemsmith
       end
 
       def initialize gem_spec: Gemsmith::Wrappers::GemSpec.new(self.class.gem_spec_path),
-                     gem_config: Gem::ConfigFile.new([]),
+                     credentials: Gemsmith::Credentials,
                      publisher: Milestoner::Publisher.new,
                      shell: Bundler::UI::Shell.new,
                      kernel: Kernel
 
         @gem_spec = gem_spec
-        @gem_config = gem_config
+        @credentials = credentials
         @publisher = publisher
         @shell = shell
         @kernel = kernel
       end
 
       def push
-        return false unless gem_credentials? && gem_credential_value?
+        creds = credentials.new key: gem_spec.allowed_push_key.to_sym, url: gem_spec.allowed_push_host
+        creds.create
 
-        options = %(--key "#{translated_api_key}" --host "#{gem_spec.allowed_push_host}")
+        options = %(--key "#{translate_key creds.key}" --host "#{gem_spec.allowed_push_host}")
         kernel.system %(gem push "pkg/#{gem_spec.package_file_name}" #{options})
         shell.confirm "Pushed #{gem_spec.package_file_name} to #{gem_spec.allowed_push_host}."
         true
@@ -41,25 +43,10 @@ module Gemsmith
 
       private
 
-      attr_reader :gem_spec, :gem_config, :publisher, :shell, :kernel
+      attr_reader :gem_spec, :credentials, :publisher, :shell, :kernel
 
-      def gem_credentials?
-        return true if File.exist?(gem_config.credentials_path)
-        shell.error "Unable to load gem credentials: #{gem_config.credentials_path}."
-        false
-      end
-
-      def gem_credential_value?
-        value = gem_config.api_keys[translated_api_key]
-        return true unless value.nil? || value.empty?
-
-        shell.error %(Invalid credential (#{gem_config.credentials_path}): :#{gem_spec.allowed_push_key}: "#{value}".)
-        false
-      end
-
-      def translated_api_key
-        return :rubygems if gem_spec.allowed_push_key == "rubygems_api_key"
-        gem_spec.allowed_push_key.to_sym
+      def translate_key key
+        key == credentials.default_key ? :rubygems : key
       end
     end
   end
--- spec/lib/gemsmith/rake/release_spec.rb
+++ spec/lib/gemsmith/rake/release_spec.rb
@@ -5,23 +5,18 @@ require "gemsmith/rake/release"
 
 RSpec.describe Gemsmith::Rake::Release, :temp_dir do
   let(:fixtures_dir) { File.join File.dirname(__FILE__), "..", "..", "..", "support", "fixtures" }
-  let(:gem_credentials_path) { File.join temp_dir, "credentials" }
   let(:gem_spec_path) { File.join fixtures_dir, "tester-no_metadata.gemspec" }
   let(:gem_spec) { Gemsmith::Wrappers::GemSpec.new gem_spec_path }
-  let(:gem_config) { Gem::ConfigFile.new [] }
+  let :credentials do
+    instance_spy Gemsmith::Credentials,
+                 key: gem_spec.allowed_push_key.to_sym,
+                 url: gem_spec.allowed_push_host
+  end
   let(:publisher) { instance_spy Milestoner::Publisher }
   let(:shell) { instance_spy Bundler::UI::Shell }
   let(:kernel) { class_spy Kernel }
-  subject do
-    described_class.new gem_spec: gem_spec,
-                        gem_config: gem_config,
-                        publisher: publisher,
-                        shell: shell,
-                        kernel: kernel
-  end
-  before do
-    allow(gem_config).to receive(:credentials_path).and_return(File.join(Dir.pwd, "tmp", "rspec", "credentials"))
-  end
+  subject { described_class.new gem_spec: gem_spec, publisher: publisher, shell: shell, kernel: kernel }
+  before { allow(Gemsmith::Credentials).to receive(:new).and_return(credentials) }
 
   describe ".gem_spec_path" do
     context "with gemspec" do
@@ -40,126 +35,56 @@ RSpec.describe Gemsmith::Rake::Release, :temp_dir do
   end
 
   describe "#push" do
-    context "with RubyGems credentials" do
-      before do
-        File.open(gem_credentials_path, "w") { |file| file.write ":rubygems_api_key: ABC" }
-        FileUtils.chmod 0600, gem_credentials_path
+    shared_examples_for "a default setup" do
+      it "pushes gem to gem server" do
+        subject.push
+        command = %(gem push "pkg/tester-0.1.0.gem" --key "rubygems" --host "https://rubygems.org")
+
+        expect(kernel).to have_received(:system).with(command)
       end
 
-      shared_examples_for "a standard setup" do
-        it "doesn't print errors" do
-          subject.push
-          expect(shell).to_not have_received(:error)
-        end
+      it "prints successful gem push message" do
+        subject.push
+        message = "Pushed tester-0.1.0.gem to https://rubygems.org."
 
-        it "pushes gem to gem server" do
-          subject.push
-          command = %(gem push "pkg/tester-0.1.0.gem" --key "rubygems" --host "https://rubygems.org")
-
-          expect(kernel).to have_received(:system).with(command)
-        end
-
-        it "prints successful gem push message" do
-          subject.push
-          message = "Pushed tester-0.1.0.gem to https://rubygems.org."
-
-          expect(shell).to have_received(:confirm).with(message)
-        end
-
-        it "answers true" do
-          expect(subject.push).to eq(true)
-        end
+        expect(shell).to have_received(:confirm).with(message)
       end
 
-      context "with RubyGems gemspec metadata" do
-        let(:gem_spec_path) { File.join fixtures_dir, "tester-only_ruby_gems_metadata.gemspec" }
-        it_behaves_like "a standard setup"
-      end
-
-      context "without gemspec metadata" do
-        let(:gem_spec_path) { File.join fixtures_dir, "tester-no_metadata.gemspec" }
-        it_behaves_like "a standard setup"
+      it "answers true" do
+        expect(subject.push).to eq(true)
       end
     end
 
-    context "with custom gem credentials" do
-      before do
-        File.open(gem_credentials_path, "w") { |file| file.write ":test: TEST" }
-        FileUtils.chmod 0600, gem_credentials_path
+    context "with RubyGems gemspec metadata" do
+      let(:gem_spec_path) { File.join fixtures_dir, "tester-only_ruby_gems_metadata.gemspec" }
+      it_behaves_like "a default setup"
+    end
+
+    context "with custom gemspec metadata" do
+      let(:gem_spec_path) { File.join fixtures_dir, "tester-custom_metadata.gemspec" }
+
+      it "pushes gem to gem server" do
+        subject.push
+        command = %(gem push "pkg/tester-0.1.0.gem" --key "test" --host "https://www.test.com")
+
+        expect(kernel).to have_received(:system).with(command)
       end
 
-      context "with custom gemspec metadata" do
-        let(:gem_spec_path) { File.join fixtures_dir, "tester-custom_metadata.gemspec" }
+      it "prints successful gem push message" do
+        subject.push
+        message = "Pushed tester-0.1.0.gem to https://www.test.com."
 
-        it "doesn't print errors" do
-          subject.push
-          expect(shell).to_not have_received(:error)
-        end
-
-        it "pushes gem to gem server" do
-          subject.push
-          command = %(gem push "pkg/tester-0.1.0.gem" --key "test" --host "https://www.test.com")
-
-          expect(kernel).to have_received(:system).with(command)
-        end
-
-        it "prints successful gem push message" do
-          subject.push
-          message = "Pushed tester-0.1.0.gem to https://www.test.com."
-
-          expect(shell).to have_received(:confirm).with(message)
-        end
-
-        it "answers true" do
-          expect(subject.push).to eq(true)
-        end
+        expect(shell).to have_received(:confirm).with(message)
       end
 
-      context "without gemspec metadata" do
-        let(:gem_spec_path) { File.join fixtures_dir, "tester-no_metadata.gemspec" }
-
-        it "prints invalid credential error" do
-          subject.push
-          message = %(Invalid credential (#{gem_credentials_path}): :rubygems_api_key: "".)
-
-          expect(shell).to have_received(:error).with(message)
-        end
-
-        it "doesn't push gem to gem server" do
-          subject.push
-          expect(kernel).to_not have_received(:system)
-        end
-
-        it "doesn't print successful gem push message" do
-          subject.push
-          expect(shell).to_not have_received(:confirm)
-        end
-
-        it "answers false" do
-          expect(subject.push).to eq(false)
-        end
+      it "answers true" do
+        expect(subject.push).to eq(true)
       end
     end
 
-    context "without gem credentials" do
-      it "prints file load error" do
-        subject.push
-        expect(shell).to have_received(:error).with("Unable to load gem credentials: #{gem_credentials_path}.")
-      end
-
-      it "doesn't push gem to gem server" do
-        subject.push
-        expect(kernel).to_not have_received(:system)
-      end
-
-      it "doesn't print successful gem push message" do
-        subject.push
-        expect(shell).to_not have_received(:confirm)
-      end
-
-      it "answers false" do
-        expect(subject.push).to eq(false)
-      end
+    context "without gemspec metadata" do
+      let(:gem_spec_path) { File.join fixtures_dir, "tester-no_metadata.gemspec" }
+      it_behaves_like "a default setup"
     end
   end
 
