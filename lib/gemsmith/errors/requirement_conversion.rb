ff00607b3781d688a731a2bb8b7c6eb983f1de27
Brooke Kuhlmann <brooke@alchemists.io>
2016-05-15 10:00:00 -0600

    Added gem requirement errors.
    - Will be thrown when initializing or casting an invalid
      gem requirement.
    - Gem requirement implementation will be added in a future commit.

---
+++ lib/gemsmith/errors/requirement_conversion.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Gemsmith
+  module Errors
+    # Thrown when attempting to convert (cast) a primitive to a gem requirement.
+    class RequirementConversion < Base
+    end
+  end
+end
---
+++ lib/gemsmith/errors/requirement_operator.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Gemsmith
+  module Errors
+    # Error class for gem requirement operator errors.
+    class RequirementOperator < Base
+    end
+  end
+end
