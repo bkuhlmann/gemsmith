6fdaf867ba3c978857555621d990a6ef8a46cf90
Brooke Kuhlmann <brooke@alchemists.io>
2016-11-05 15:53:02 -0600

    Added gem inspector.
    - Provides a specialized object which can inspect gem source code.
    - This will eventually replace the functionality provided by the
      `Gemsmith::Gem::Specification`.

---
+++ spec/lib/gemsmith/gem/inspector_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Gemsmith::Gem::Inspector do
+  let(:fixtures_dir) { File.join File.dirname(__FILE__), "..", "..", "..", "support", "fixtures" }
+  let(:specification_path) { File.join fixtures_dir, "tester-homepage_url.gemspec" }
+  let(:specification) { Gemsmith::Gem::Specification.new specification_path }
+  let(:shell) { class_spy Open3 }
+
+  subject { described_class.new shell: shell }
+
+  describe ".editor" do
+    it "answers current editor/IDE for editing source code" do
+      ClimateControl.modify EDITOR: "sublime" do
+        expect(described_class.editor).to eq("sublime")
+      end
+    end
+  end
+
+  describe "#edit" do
+    it "edits gem source code" do
+      subject.edit specification
+      expect(shell).to have_received(:capture2).with(described_class.editor, /.+tester\-0\.1\.0/)
+    end
+  end
+
+  describe "#visit" do
+    it "visits gem home page" do
+      subject.visit specification
+      expect(shell).to have_received(:capture2).with("open", "https://www.example.com")
+    end
+  end
+end
