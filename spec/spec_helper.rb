6c1eff3affca65bf8f7b7a39ce398b5b0247d607
Brooke Kuhlmann <brooke@alchemists.io>
2019-04-16 20:16:39 -0600

    Updated RSpec helper to verify constant names.
    Ensures string constants when used as doubles are verified to protect
    against misspellings. [Details](https://is.gd/p1M8nU).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,13 +19,17 @@ RSpec.configure do |config|
   config.example_status_persistence_file_path = "./tmp/rspec-examples.txt"
   config.filter_run_when_matching :focus
   config.formatter = ENV["CI"] == "true" ? :progress : :documentation
-  config.mock_with(:rspec) { |mocks| mocks.verify_partial_doubles = true }
-  config.order = "random"
+  config.order = :random
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.warnings = true
 
   config.expect_with :rspec do |expectations|
     expectations.syntax = :expect
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_doubled_constant_names = true
+    mocks.verify_partial_doubles = true
   end
 end
