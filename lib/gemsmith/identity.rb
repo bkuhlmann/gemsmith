394ca34bf990d30d9bd66659419f231a56c1b20a
Brooke Kuhlmann <brooke@alchemists.io>
2020-11-21 13:20:15 -0700

    Added version release notes

--- lib/gemsmith/identity.rb
+++ lib/gemsmith/identity.rb
@@ -5,7 +5,7 @@ module Gemsmith
   module Identity
     NAME = "gemsmith"
     LABEL = "Gemsmith"
-    VERSION = "14.8.0"
+    VERSION = "14.9.0"
     VERSION_LABEL = "#{LABEL} #{VERSION}"
     URL = "https://www.alchemists.io/projects/gemsmith"
   end
