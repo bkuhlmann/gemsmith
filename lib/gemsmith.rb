864d0528c20371dee8d23350ea45d5afc8f50c86
Brooke Kuhlmann <brooke@alchemists.io>
2021-11-23 15:52:23 -0700

    Added Rubysmith
    Necessary to build upon the Rubysmith architecture which will soon
    replace a lot of duplicated effort originally found in this project.

--- lib/gemsmith.rb
+++ lib/gemsmith.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "zeitwerk"
+require "rubysmith"
 
 Zeitwerk::Loader.for_gem.then do |loader|
   loader.inflector.inflect "cli" => "CLI", "circle_ci" => "CircleCI"
