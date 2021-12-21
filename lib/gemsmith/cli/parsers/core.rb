48e3a57cecca44199093a343050ab948acf9ca62
Brooke Kuhlmann <brooke@alchemists.io>
2021-12-21 15:39:17 -0700

    Added CLI core parser
    Necessary to process the core command line options for the shell.

---
+++ lib/gemsmith/cli/parsers/core.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+require "pathname"
+require "refinements/structs"
+
+module Gemsmith
+  module CLI
+    module Parsers
+      # Handles parsing of Command Line Interface (CLI) core options.
+      class Core
+        using Refinements::Structs
+
+        def self.call(...) = new(...).call
+
+        def initialize configuration = Container[:configuration],
+                       client: Parser::CLIENT,
+                       container: Container
+          @configuration = configuration
+          @client = client
+          @container = container
+        end
+
+        def call arguments = []
+          client.banner = specification.labeled_summary
+          client.separator "\nUSAGE:\n"
+          collate
+          client.parse arguments
+          configuration
+        end
+
+        private
+
+        attr_reader :configuration, :client, :container
+
+        def collate = private_methods.sort.grep(/add_/).each { |method| __send__ method }
+
+        def add_config
+          client.on "-c",
+                    "--config ACTION",
+                    %i[edit view],
+                    "Manage gem configuration: edit or view." do |action|
+            configuration.merge! action_config: action
+          end
+        end
+
+        def add_build
+          client.on "-b", "--build NAME [options]", "Build new project." do |name|
+            configuration.merge! action_build: true, project_name: name
+          end
+        end
+
+        def add_edit
+          client.on "--edit GEM", "Edit installed gem in default editor." do |gem_name|
+            configuration.merge! action_edit: gem_name
+          end
+        end
+
+        def add_install
+          client.on "-i", "--install [NAME]", "Install gem for local development." do |name|
+            configuration.merge! action_install: true,
+                                 project_name: name || Pathname.pwd.basename.to_s
+          end
+        end
+
+        def add_publish
+          client.on "-p", "--publish [NAME]", "Publish gem to remote gem server." do |name|
+            configuration.merge! action_publish: true,
+                                 project_name: name || Pathname.pwd.basename.to_s
+          end
+        end
+
+        def add_view
+          client.on "--view GEM", "View installed gem in default browser." do |gem_name|
+            configuration.merge! action_view: gem_name
+          end
+        end
+
+        def add_version
+          client.on "-v", "--version", "Show gem version." do
+            configuration.merge! action_version: true
+          end
+        end
+
+        def add_help
+          client.on "-h", "--help", "Show this message." do
+            configuration.merge! action_help: true
+          end
+        end
+
+        def specification = container[__method__]
+      end
+    end
+  end
+end
---
+++ spec/lib/gemsmith/cli/parsers/core_spec.rb
@@ -0,0 +1,147 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Gemsmith::CLI::Parsers::Core do
+  subject(:parser) { described_class.new test_configuration }
+
+  include_context "with application container"
+
+  it_behaves_like "a parser"
+
+  describe "#call" do
+    let(:test_configuration) { configuration.minimize.dup }
+
+    it "answers config edit (short)" do
+      expect(parser.call(%w[-c edit])).to have_attributes(action_config: :edit)
+    end
+
+    it "answers config edit (long)" do
+      expect(parser.call(%w[--config edit])).to have_attributes(action_config: :edit)
+    end
+
+    it "answers config view (short)" do
+      expect(parser.call(%w[-c view])).to have_attributes(action_config: :view)
+    end
+
+    it "answers config view (long)" do
+      expect(parser.call(%w[--config view])).to have_attributes(action_config: :view)
+    end
+
+    it "fails with missing config action" do
+      expectation = proc { parser.call %w[--config] }
+      expect(&expectation).to raise_error(OptionParser::MissingArgument, /--config/)
+    end
+
+    it "fails with invalid config action" do
+      expectation = proc { parser.call %w[--config bogus] }
+      expect(&expectation).to raise_error(OptionParser::InvalidArgument, /bogus/)
+    end
+
+    it "answers build (short)" do
+      expect(parser.call(%w[-b test])).to have_attributes(action_build: true, project_name: "test")
+    end
+
+    it "answers build (long)" do
+      expect(parser.call(%w[--build test])).to have_attributes(
+        action_build: true,
+        project_name: "test"
+      )
+    end
+
+    it "fails with missing build name" do
+      expectation = proc { parser.call %w[--build] }
+      expect(&expectation).to raise_error(OptionParser::MissingArgument, /--build/)
+    end
+
+    it "answers edit" do
+      expect(parser.call(%w[--edit test])).to have_attributes(action_edit: "test")
+    end
+
+    it "fails with missing editable gem name" do
+      expectation = proc { parser.call %w[--edit] }
+      expect(&expectation).to raise_error(OptionParser::MissingArgument, /--edit/)
+    end
+
+    it "answers install (short) with default project name" do
+      expect(parser.call(%w[-i])).to have_attributes(
+        action_install: true,
+        project_name: kind_of(String)
+      )
+    end
+
+    it "answers install (long) with default project name" do
+      expect(parser.call(%w[--install])).to have_attributes(
+        action_install: true,
+        project_name: kind_of(String)
+      )
+    end
+
+    it "answers install (short) with custom project name)" do
+      expect(parser.call(%w[-i test])).to have_attributes(
+        action_install: true,
+        project_name: "test"
+      )
+    end
+
+    it "answers install (long) with custom project name" do
+      expect(parser.call(%w[--install test])).to have_attributes(
+        action_install: true,
+        project_name: "test"
+      )
+    end
+
+    it "answers publish (short) with default project name" do
+      expect(parser.call(%w[-p])).to have_attributes(
+        action_publish: true,
+        project_name: kind_of(String)
+      )
+    end
+
+    it "answers publish (long) with default project name" do
+      expect(parser.call(%w[--publish])).to have_attributes(
+        action_publish: true,
+        project_name: kind_of(String)
+      )
+    end
+
+    it "answers publish (short) with custom project name)" do
+      expect(parser.call(%w[-p test])).to have_attributes(
+        action_publish: true,
+        project_name: "test"
+      )
+    end
+
+    it "answers publish (long) with custom project name" do
+      expect(parser.call(%w[--publish test])).to have_attributes(
+        action_publish: true,
+        project_name: "test"
+      )
+    end
+
+    it "answers view" do
+      expect(parser.call(%w[--view test])).to have_attributes(action_view: "test")
+    end
+
+    it "fails with missing viewable gem name" do
+      expectation = proc { parser.call %w[--view] }
+      expect(&expectation).to raise_error(OptionParser::MissingArgument, /--view/)
+    end
+
+    it "answers version (short)" do
+      expect(parser.call(%w[-v])).to have_attributes(action_version: true)
+    end
+
+    it "answers version (long)" do
+      expect(parser.call(%w[--version])).to have_attributes(action_version: true)
+    end
+
+    it "enables help (short)" do
+      expect(parser.call(%w[-h])).to have_attributes(action_help: true)
+    end
+
+    it "enables help (long)" do
+      expect(parser.call(%w[--help])).to have_attributes(action_help: true)
+    end
+  end
+end
