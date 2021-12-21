46580c5fdf65c27b9ee1531331c23a982a26a2f7
Brooke Kuhlmann <brooke@alchemists.io>
2021-12-21 15:48:49 -0700

    Added CLI build action
    Necessary to process the build action based on shell input. This builds
    upon a lot of what is provided by equivalent Rubysmith build action.

---
+++ lib/gemsmith/cli/actions/build.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module Gemsmith
+  module CLI
+    module Actions
+      # Handles the build action.
+      class Build
+        # Order is important.
+        BUILDERS = [
+          Rubysmith::Builders::Core,
+          Rubysmith::Builders::Version,
+          Builders::Specification,
+          Rubysmith::Builders::Documentation::Readme,
+          Rubysmith::Builders::Documentation::Citation,
+          Rubysmith::Builders::Documentation::License,
+          Rubysmith::Builders::Documentation::Version,
+          Rubysmith::Builders::Git::Setup,
+          Rubysmith::Builders::Git::Ignore,
+          Builders::Git::Ignore,
+          Rubysmith::Builders::Bundler,
+          Builders::Bundler,
+          Builders::CLI,
+          Rubysmith::Builders::Rake,
+          Rubysmith::Builders::Console,
+          Rubysmith::Builders::CircleCI,
+          Rubysmith::Builders::Setup,
+          Rubysmith::Builders::GitHub,
+          Rubysmith::Builders::Guard,
+          Rubysmith::Builders::Reek,
+          Rubysmith::Builders::RSpec::Context,
+          Rubysmith::Builders::RSpec::Helper,
+          Builders::RSpec::Helper,
+          Rubysmith::Builders::Rubocop,
+          Rubysmith::Extensions::Bundler,
+          Rubysmith::Extensions::Pragmater,
+          Rubysmith::Extensions::Tocer,
+          Rubysmith::Extensions::Rubocop,
+          Builders::Git::Commit
+        ].freeze
+
+        def initialize builders: BUILDERS
+          @builders = builders
+        end
+
+        def call(configuration) = builders.each { |builder| builder.call configuration }
+
+        private
+
+        attr_reader :configuration, :builders
+      end
+    end
+  end
+end
---
+++ spec/lib/gemsmith/cli/actions/build_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Gemsmith::CLI::Actions::Build do
+  subject(:action) { described_class.new builders: [builder] }
+
+  include_context "with application container"
+
+  let(:builder) { class_spy Gemsmith::Builders::Specification }
+
+  describe "#call" do
+    it "calls builders" do
+      action.call configuration
+      expect(builder).to have_received(:call).with(configuration)
+    end
+  end
+end
