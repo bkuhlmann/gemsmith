67cedb593a622312a02b0d99fe5e22bec0274043
Brooke Kuhlmann <brooke@alchemists.io>
2021-12-21 15:01:29 -0700

    Added Git ignore builder
    Necessary to build Git support with information specific for gem
    projects. For now, this is only the ignore configuration but could
    expand further in the future.

---
+++ lib/gemsmith/builders/git/ignore.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "refinements/structs"
+
+module Gemsmith
+  module Builders
+    module Git
+      # Builds project skeleton Git ignore.
+      class Ignore
+        using Refinements::Structs
+
+        def self.call(...) = new(...).call
+
+        def initialize configuration, builder: Rubysmith::Builder
+          @configuration = configuration
+          @builder = builder
+        end
+
+        def call
+          return configuration unless configuration.build_git
+
+          builder.call(configuration.merge(template_path: "%project_name%/.gitignore.erb"))
+                 .touch
+                 .prepend("*.gem\n")
+                 .insert_before("tmp\n", "Gemfile.lock\n")
+                 .insert_before("tmp\n", "pkg\n")
+
+          configuration
+        end
+
+        private
+
+        attr_reader :configuration, :builder
+      end
+    end
+  end
+end
---
+++ spec/lib/gemsmith/builders/git/ignore_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Gemsmith::Builders::Git::Ignore do
+  using Refinements::Pathnames
+  using Refinements::Structs
+
+  subject(:builder) { described_class.new test_configuration }
+
+  include_context "with application container"
+
+  it_behaves_like "a builder"
+
+  describe "#call" do
+    context "with enabled" do
+      let(:test_configuration) { configuration.minimize.merge build_git: true }
+
+      before do
+        temp_dir.join("test").make_path.join(".gitignore").write(<<~CONTENT)
+          .bundle
+          .rubocop-http*
+          .yardoc
+          doc/yard
+          tmp
+        CONTENT
+      end
+
+      it "builds Git ignore" do
+        builder.call
+
+        expect(temp_dir.join("test/.gitignore").read).to eq(<<~CONTENT)
+          *.gem
+          .bundle
+          .rubocop-http*
+          .yardoc
+          doc/yard
+          Gemfile.lock
+          pkg
+          tmp
+        CONTENT
+      end
+    end
+
+    context "with disabled" do
+      let(:test_configuration) { configuration.minimize }
+
+      it "does not build file" do
+        builder.call
+        expect(temp_dir.join("test/.gitignore").exist?).to eq(false)
+      end
+    end
+  end
+end
