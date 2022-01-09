738bdd46d46e1cc08777f87dc10d67abf4d3bdae
Brooke Kuhlmann <brooke@alchemists.io>
2022-01-09 08:28:45 -0700

    Added cleaner tool
    Necessary to ensure any gem artifacts are properly cleaned up for a
    project.

---
+++ lib/gemsmith/tools/cleaner.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "dry/monads"
+require "refinements/pathnames"
+
+module Gemsmith
+  module Tools
+    # Cleans gem artifacts.
+    class Cleaner
+      include Dry::Monads[:result]
+
+      using Refinements::Pathnames
+
+      def initialize root_dir: Pathname.pwd
+        @root_dir = root_dir
+      end
+
+      def call specification
+        root_dir.join("pkg").remove_tree
+        root_dir.files("**/*.gem").each(&:delete)
+        Success specification
+      end
+
+      private
+
+      attr_reader :root_dir
+    end
+  end
+end
---
+++ spec/lib/gemsmith/tools/cleaner_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Gemsmith::Tools::Cleaner do
+  using Refinements::Pathnames
+
+  subject(:cleaner) { described_class.new }
+
+  include_context "with application container"
+
+  describe "#call" do
+    it "deletes Gemsmith artifacts" do
+      temp_dir.change_dir do
+        path = temp_dir.join("tmp/gemsmith-tester-0.0.0.gem").deep_touch
+        cleaner.call specification
+
+        expect(path.exist?).to eq(false)
+      end
+    end
+
+    it "deletes Bundler artifacts" do
+      temp_dir.change_dir do
+        temp_dir.join("pkg/gemsmith-tester-0.0.0.gem").deep_touch
+        cleaner.call specification
+
+        expect(temp_dir.join("pkg").exist?).to eq(false)
+      end
+    end
+
+    it "deletes extraneous root artifacts" do
+      temp_dir.change_dir do
+        path = temp_dir.join("gemsmith-tester-0.0.0.gem").touch
+        cleaner.call specification
+
+        expect(path.exist?).to eq(false)
+      end
+    end
+  end
+end
