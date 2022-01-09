19456fb5a25fd803d32da1d1fb3e36f06f82ab07
Brooke Kuhlmann <brooke@alchemists.io>
2022-01-09 11:34:49 -0700

    Added gem picker
    Necessary to allow one to pick a gem for further inspection. This will
    soon be used to edit or view a gem. Behavior is determined by number of
    matches:
    
    - **One**: Gem is automatically selected and prompt is skipped.
    - **Many**: A prompt is presented for selection.
    - **Zero**: An error message is provided.

---
+++ lib/gemsmith/gems/picker.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "dry/monads"
+
+module Gemsmith
+  module Gems
+    # Picks a gem specification.
+    class Picker
+      include Dry::Monads[:result]
+
+      def self.call(name, ...) = new(...).call name
+
+      def initialize finder: Finder.new, kernel: Kernel
+        @finder = finder
+        @kernel = kernel
+      end
+
+      def call name
+        specifications = finder.call name
+
+        case specifications.size
+          when 1 then Success specifications.first
+          when 2.. then Success choose(specifications)
+          else Failure "Unknown gem or gem is not installed: #{name}."
+        end
+      end
+
+      def choose specifications
+        specifications.each.with_index 1 do |specification, index|
+          kernel.puts "#{index}. #{specification.named_version}"
+        end
+
+        kernel.puts "\nPlease enter gem selection:"
+        ARGV.clear
+        specifications[kernel.gets.chomp.to_i - 1]
+      end
+
+      private
+
+      attr_reader :finder, :kernel
+    end
+  end
+end
---
+++ spec/lib/gemsmith/gems/picker_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Gemsmith::Gems::Picker do
+  subject(:picker) { described_class.new finder:, kernel: }
+
+  let(:kernel) { class_spy Kernel }
+  let(:finder) { instance_double Gemsmith::Gems::Finder }
+  let(:fixture_path) { Bundler.root.join "spec/support/fixtures/gemsmith-tester.gemspec" }
+
+  describe ".call" do
+    before { allow(finder).to receive(:call).and_return([specification]) }
+
+    let(:specification) { Gemsmith::Gems::Loader.call fixture_path }
+
+    it "answers specification" do
+      result = described_class.call("logger", finder:, kernel:)
+      expect(result.success).to eq(specification)
+    end
+  end
+
+  describe "#call" do
+    context "with single selection" do
+      before { allow(finder).to receive(:call).and_return([specification]) }
+
+      let(:specification) { Gemsmith::Gems::Loader.call fixture_path }
+
+      it "answers computed specification" do
+        result = picker.call "logger"
+        expect(result.success).to eq(specification)
+      end
+    end
+
+    context "with multiple selections" do
+      before do
+        allow(finder).to receive(:call).and_return([specification_a, specification_b])
+        allow(kernel).to receive(:gets).and_return("2")
+      end
+
+      let(:specification_a) { Gemsmith::Gems::Loader.call fixture_path }
+      let(:specification_b) { Gemsmith::Gems::Loader.call fixture_path }
+
+      it "answers chosen specification" do
+        result = picker.call "logger"
+        expect(result.success).to eq(specification_b)
+      end
+    end
+
+    context "with zero selections" do
+      before { allow(finder).to receive(:call).and_return([]) }
+
+      it "fails with message" do
+        result = picker.call "test"
+        expect(result.failure).to eq("Unknown gem or gem is not installed: test.")
+      end
+    end
+  end
+end
