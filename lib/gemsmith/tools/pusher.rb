165b1983fc04716788d17192bb66b064a198d10a
Brooke Kuhlmann <brooke@alchemists.io>
2022-03-31 17:35:04 -0600

    Fixed gem push exception when YubiKey Manager can't be found
    Necessary to prevent the following error:
    
        No such file or directory - ykman (Errno::ENOENT)
    
    This occurs on systems which don't have the YubiKey Manager installed.
    This also catches situations in which `command` -- used to detect
    `ykman` -- can't be found either.

--- lib/gemsmith/tools/pusher.rb
+++ lib/gemsmith/tools/pusher.rb
@@ -7,7 +7,7 @@ module Gemsmith
   module Tools
     # Pushes a gem package to remote gem server.
     class Pusher
-      include Import[:executor]
+      include Import[:executor, :logger]
       include Dry::Monads[:result]
 
       def initialize command: Gem::CommandManager.new, **dependencies
@@ -27,9 +27,27 @@ module Gemsmith
 
       attr_reader :command
 
+      # :reek:TooManyStatements
       def one_time_password
-        executor.capture3("ykman", "oath", "accounts", "code", "--single", "RubyGems")
+        return [] if check_yubikey.failure?
+
+        executor.capture3(check_yubikey.success, "oath", "accounts", "code", "--single", "RubyGems")
                 .then { |stdout, _stderr, status| status.success? ? ["--otp", stdout.chomp] : [] }
+      rescue Errno::ENOENT => error
+        logger.warn { "Unable to obtain YubiKey One-Time Password. #{error}." }
+        []
+      end
+
+      def check_yubikey
+        executor.capture3("command", "-v", "ykman")
+                .then do |stdout, stderr, status|
+                  if status.success?
+                    Success stdout.chomp
+                  else
+                    logger.warn { "Unable to find YubiKey Manager. #{stderr}." }
+                    Failure()
+                  end
+                end
       end
     end
   end
--- spec/lib/gemsmith/tools/pusher_spec.rb
+++ spec/lib/gemsmith/tools/pusher_spec.rb
@@ -10,34 +10,86 @@ RSpec.describe Gemsmith::Tools::Pusher do
   let(:command) { instance_spy Gem::CommandManager, run: nil }
 
   describe "#call" do
-    let(:executor) { class_double Open3, capture3: ["abc", "Unknown!", status] }
-    let(:status) { instance_double Process::Status, success?: true }
+    let(:executor) { class_double Open3, capture3: ["abc", "Unknown", status] }
+    let(:status) { instance_double Process::Status }
 
-    it "answers specification when success" do
-      result = pusher.call specification
-      expect(result.success).to eq(specification)
+    context "when YubiKey Manager exists and One-Time Password is obtained" do
+      before { allow(status).to receive(:success?).and_return(true, true) }
+
+      it "pushes with One-Time Password" do
+        pusher.call specification
+
+        expect(command).to have_received(:run).with(
+          ["push", "tmp/gemsmith-test-0.0.0.gem", "--otp", "abc"]
+        )
+      end
+
+      it "answers specification" do
+        result = pusher.call specification
+        expect(result.success).to eq(specification)
+      end
+
+      it "answers error message with gem exception failure" do
+        allow(command).to receive(:run).and_raise Gem::Exception, "Exception!"
+        result = pusher.call specification
+
+        expect(result.failure).to eq("Exception!")
+      end
     end
 
-    it "uses One-Time Password when obtained" do
-      pusher.call specification
+    context "when YubiKey Manager exists and One-Time Password can't be obtained" do
+      before { allow(status).to receive(:success?).and_return(true, false) }
 
-      expect(command).to have_received(:run).with(
-        ["push", "tmp/gemsmith-test-0.0.0.gem", "--otp", "abc"]
-      )
+      it "pushes without One-Time Password" do
+        pusher.call specification
+        expect(command).to have_received(:run).with(["push", "tmp/gemsmith-test-0.0.0.gem"])
+      end
+
+      it "answers specification" do
+        result = pusher.call specification
+        expect(result.success).to eq(specification)
+      end
     end
 
-    it "doesn't use One-Time Password when it can't be obtained" do
-      allow(status).to receive(:success?).and_return(false)
-      pusher.call specification
+    context "when YubiKey Manager doesn't exist" do
+      before { allow(status).to receive(:success?).and_return(false, true) }
 
-      expect(command).to have_received(:run).with(["push", "tmp/gemsmith-test-0.0.0.gem"])
+      it "pushes without One-Time Password" do
+        pusher.call specification
+        expect(command).to have_received(:run).with(["push", "tmp/gemsmith-test-0.0.0.gem"])
+      end
+
+      it "logs warning" do
+        pusher.call specification
+        expect(logger.reread).to eq("Unable to find YubiKey Manager. Unknown.\n")
+      end
+
+      it "answers specification" do
+        result = pusher.call specification
+        expect(result.success).to eq(specification)
+      end
     end
 
-    it "answers error message when failure" do
-      allow(command).to receive(:run).and_raise Gem::Exception, "Exception!"
-      result = pusher.call specification
+    context "when system error is raised" do
+      before { allow(executor).to receive(:capture3).and_raise(Errno::ENOENT, "Danger") }
 
-      expect(result.failure).to eq("Exception!")
+      it "pushes without One-Time Password" do
+        pusher.call specification
+        expect(command).to have_received(:run).with(["push", "tmp/gemsmith-test-0.0.0.gem"])
+      end
+
+      it "logs warning" do
+        pusher.call specification
+
+        expect(logger.reread).to eq(
+          "Unable to obtain YubiKey One-Time Password. No such file or directory - Danger.\n"
+        )
+      end
+
+      it "answers specification" do
+        result = pusher.call specification
+        expect(result.success).to eq(specification)
+      end
     end
   end
 end
