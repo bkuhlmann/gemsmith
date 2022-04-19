e397a2e2558d8c91e57f47dd788529fdc37f81de
Brooke Kuhlmann <brooke@alchemists.io>
2022-04-19 09:24:40 -0600

    Updated to Caliber 0.6.0
    Necessary to pick up latest RuboCop RSpec enhancements and fixes.

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gemspec
 
 group :code_quality do
   gem "bundler-leak", "~> 0.2"
-  gem "caliber", "~> 0.5"
+  gem "caliber", "~> 0.6"
   gem "git-lint", "~> 4.0"
   gem "reek", "~> 6.1"
   gem "simplecov", "~> 0.21"
--- spec/lib/gemsmith/builders/bundler_spec.rb
+++ spec/lib/gemsmith/builders/bundler_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe Gemsmith::Builders::Bundler do
 
           group :code_quality do
             gem "bundler-leak", "~> 0.2"
-            gem "caliber", "~> 0.5"
+            gem "caliber", "~> 0.6"
             gem "git-lint", "~> 4.0"
             gem "reek", "~> 6.1"
             gem "simplecov", "~> 0.21"
