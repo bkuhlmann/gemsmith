63f0f8610434375ce2bd96732cab2e97a77edc00
Brooke Kuhlmann <brooke@alchemists.io>
2022-07-14 19:45:58 -0600

    Updated to Caliber 0.11.0
    [Details](https://www.alchemists.io/projects/caliber).

--- spec/lib/gemsmith/builders/bundler_spec.rb
+++ spec/lib/gemsmith/builders/bundler_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe Gemsmith::Builders::Bundler do
 
           group :code_quality do
             gem "bundler-leak", "~> 0.2"
-            gem "caliber", "~> 0.9"
+            gem "caliber", "~> 0.11"
             gem "git-lint", "~> 4.0"
             gem "reek", "~> 6.1"
             gem "simplecov", "~> 0.21"
