023a6564cc14845deb50b4e90a6e06bfe5fe0699
Brooke Kuhlmann <brooke@alchemists.io>
2022-07-31 11:58:19 -0600

    Added installer tool spec for gem install failure
    Necessary to ensure this code path is properly tested and answers the
    expected error.

--- spec/lib/gemsmith/tools/installer_spec.rb
+++ spec/lib/gemsmith/tools/installer_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Gemsmith::Tools::Installer do
   let(:cleaner) { instance_spy Gemsmith::Tools::Cleaner, call: Success() }
 
   describe "#call" do
-    context "when success" do
+    context "with success" do
       let(:steps) { [proc { Success() }, proc { Success() }] }
 
       it "installs gem" do
@@ -33,12 +33,24 @@ RSpec.describe Gemsmith::Tools::Installer do
       end
     end
 
-    context "when failure" do
+    context "with step failure" do
       let(:steps) { [proc { Failure "Danger!" }, proc { Success() }] }
 
       it "answers failure" do
         result = installer.call specification
         expect(result.failure).to eq("Danger!")
+      end
+    end
+
+    context "with install failure" do
+      let(:steps) { [proc { Success() }] }
+
+      it "answers failure" do
+        status = instance_spy Process::Status, success?: false
+        allow(executor).to receive(:capture3).and_return ["", "", status]
+
+        result = installer.call specification
+        expect(result.failure).to eq("Unable to install: tmp/gemsmith-test-0.0.0.gem.")
       end
     end
   end
