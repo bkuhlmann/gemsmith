0da5f0a3e733fc77713eae5d2372558e8905583a
Brooke Kuhlmann <brooke@alchemists.io>
2023-01-09 10:37:30 -0700

    Refactored configuration loader passing of arguments to super class
    Minor adjustment to use less lines of code.

--- lib/gemsmith/configuration/loader.rb
+++ lib/gemsmith/configuration/loader.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
-require "refinements/structs"
 require "runcom"
 
 module Gemsmith
   module Configuration
     # Represents the fully assembled Command Line Interface (CLI) configuration.
     class Loader < Rubysmith::Configuration::Loader
-      using Refinements::Structs
-
       DEFAULTS = Rubysmith::Configuration::Loader::DEFAULTS
       CLIENT = Runcom::Config.new "gemsmith/configuration.yml", defaults: DEFAULTS
 
@@ -17,9 +14,7 @@ module Gemsmith
             enhancers: {template_root: Rubysmith::Configuration::Enhancers::TemplateRoot}
       end
 
-      def initialize client: CLIENT, **dependencies
-        super
-      end
+      def initialize(client: CLIENT, **) = super
 
       def call
         return super unless enhancers.key? :template_root
