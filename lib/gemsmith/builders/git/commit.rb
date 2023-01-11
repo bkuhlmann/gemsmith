de070e118accf47bb65c59a6c7f6a66bb0208216
Brooke Kuhlmann <brooke@alchemists.io>
2023-01-10 17:57:13 -0700

    Refactored Git commit builder to inherit from Rubysmith
    Necessary to reduce duplicated effort since we can delegate to the Rubysmith implementation by only swapping about the specification dependency with the one used in this project.

--- lib/gemsmith/builders/git/commit.rb
+++ lib/gemsmith/builders/git/commit.rb
@@ -4,41 +4,8 @@ module Gemsmith
   module Builders
     module Git
       # Builds project skeleton initial Git commit message.
-      class Commit
+      class Commit < Rubysmith::Builders::Git::Commit
         include Import[:specification]
-
-        def self.call(...) = new(...).call
-
-        def initialize configuration, builder: Rubysmith::Builder, **dependencies
-          super(**dependencies)
-
-          @configuration = configuration
-          @builder = builder
-        end
-
-        def call
-          return configuration unless configuration.build_git
-
-          builder.call(configuration)
-                 .run("git add .", chdir: project_name)
-                 .run(
-                   %(git commit --all --message "Added project skeleton" --message "#{body}"),
-                   chdir: project_name
-                 )
-
-          configuration
-        end
-
-        private
-
-        attr_reader :configuration, :builder
-
-        def body
-          "Generated with [#{specification.label}](#{specification.homepage_url}) " \
-          "#{specification.version}."
-        end
-
-        def project_name = configuration.project_name
       end
     end
   end
