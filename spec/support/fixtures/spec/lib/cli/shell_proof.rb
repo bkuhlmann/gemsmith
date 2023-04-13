271fda5a7a864c1fa0f4eabd0401d57acb5f3238
Brooke Kuhlmann <brooke@alchemists.io>
2023-04-13 09:30:44 -0600

    Fixed CLI template log and standard output
    Necessary to reduce reliance on using the logger for version and usage information while also updating the specs to be more aware of the difference (including emoji/color detection).

--- spec/support/fixtures/spec/lib/cli/shell_proof.rb
+++ spec/support/fixtures/spec/lib/cli/shell_proof.rb
@@ -25,22 +25,22 @@ RSpec.describe Test::CLI::Shell do
 
     it "prints version" do
       shell.call %w[--version]
-      expect(logger.reread).to match(/Test\s\d+\.\d+\.\d+/)
+      expect(kernel).to have_received(:puts).with(/Test\s\d+\.\d+\.\d+/)
     end
 
     it "prints help (usage)" do
       shell.call %w[--help]
-      expect(logger.reread).to match(/Test.+USAGE.+/m)
+      expect(kernel).to have_received(:puts).with(/Test.+USAGE.+/m)
     end
 
     it "prints usage when no options are given" do
       shell.call
-      expect(logger.reread).to match(/Test.+USAGE.+/m)
+      expect(kernel).to have_received(:puts).with(/Test.+USAGE.+/m)
     end
 
     it "prints error with invalid option" do
       shell.call %w[--bogus]
-      expect(logger.reread).to match(/invalid option.+bogus/)
+      expect(logger.reread).to match(/🛑.+invalid option.+bogus/)
     end
   end
 end
