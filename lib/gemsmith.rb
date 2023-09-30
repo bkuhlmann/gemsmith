abe0ae44b0fbfb5b4c266053406dc2bc0762918d
Brooke Kuhlmann <brooke@alchemists.io>
2023-09-30 15:46:55 -0600

    Fixed Zeitwerk loader
    Necessary to ensure the gem is properly tagged and the default inflector is used since the gem inflector unfortunately looks for a version file which is not used by this gem.
    
    Milestone: patch

--- lib/gemsmith.rb
+++ lib/gemsmith.rb
@@ -3,8 +3,10 @@
 require "rubysmith"
 require "zeitwerk"
 
-Zeitwerk::Loader.for_gem.then do |loader|
+Zeitwerk::Loader.new.then do |loader|
   loader.inflector.inflect "cli" => "CLI", "circle_ci" => "CircleCI", "rspec" => "RSpec"
+  loader.tag = File.basename __FILE__, ".rb"
+  loader.push_dir __dir__
   loader.setup
 end
 
