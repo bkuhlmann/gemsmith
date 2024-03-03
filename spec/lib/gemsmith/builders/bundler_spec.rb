41f0756c60024f7636fd6eb6697328ac2e0c5140
Brooke Kuhlmann <brooke@alchemists.io>
2024-03-03 10:07:15 -0700

    Updated to Git Lint 7.1.0
    See link:https://alchemists.io/projects/git-lint/versions[details].
    
    Milestone: minor

--- spec/lib/gemsmith/builders/bundler_spec.rb
+++ spec/lib/gemsmith/builders/bundler_spec.rb
@@ -49,7 +49,7 @@ RSpec.describe Gemsmith::Builders::Bundler do
 
           group :quality do
             gem "caliber", "~> 0.51"
-            gem "git-lint", "~> 7.0"
+            gem "git-lint", "~> 7.1"
             gem "reek", "~> 6.3", require: false
             gem "simplecov", "~> 0.22", require: false
           end
