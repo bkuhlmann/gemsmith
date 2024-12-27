d73df9b04a2386beaa195b960c03f1dca96b19c8
Brooke Kuhlmann <brooke@alchemists.io>
2024-12-27 14:07:05 -0700

    Updated to Git Lint 9.0.0
    link:https://alchemists.io/projects/git-lint/versions[Details].
    
    Milestone: patch

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gemspec
 
 group :quality do
   gem "caliber", "~> 0.68"
-  gem "git-lint", "~> 8.7"
+  gem "git-lint", "~> 9.0"
   gem "reek", "~> 6.3", require: false
   gem "simplecov", "~> 0.22", require: false
 end
--- spec/lib/gemsmith/builders/bundler_spec.rb
+++ spec/lib/gemsmith/builders/bundler_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe Gemsmith::Builders::Bundler do
 
           group :quality do
             gem "caliber", "~> 0.68"
-            gem "git-lint", "~> 8.7"
+            gem "git-lint", "~> 9.0"
             gem "reek", "~> 6.3", require: false
             gem "simplecov", "~> 0.22", require: false
           end
