5c4d5d767592855d6d947084345ddf9f1a88939e
Brooke Kuhlmann <brooke@alchemists.io>
2024-12-27 08:39:50 -0700

    Updated to Caliber 0.68.0
    link:https://alchemists.io/projects/caliber/versions[Details].
    
    Milestone: minor

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ source "https://rubygems.org"
 gemspec
 
 group :quality do
-  gem "caliber", "~> 0.64"
+  gem "caliber", "~> 0.68"
   gem "git-lint", "~> 8.7"
   gem "reek", "~> 6.3", require: false
   gem "simplecov", "~> 0.22", require: false
--- spec/lib/gemsmith/builders/bundler_spec.rb
+++ spec/lib/gemsmith/builders/bundler_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe Gemsmith::Builders::Bundler do
           gem "bootsnap", "~> 1.18"
 
           group :quality do
-            gem "caliber", "~> 0.64"
+            gem "caliber", "~> 0.68"
             gem "git-lint", "~> 8.7"
             gem "reek", "~> 6.3", require: false
             gem "simplecov", "~> 0.22", require: false
