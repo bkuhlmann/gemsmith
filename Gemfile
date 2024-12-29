714be94337d8eece52f44e00ae1a790f20c0fd6d
Brooke Kuhlmann <brooke@alchemists.io>
2024-12-29 14:42:03 -0700

    Updated to Debug 1.10.0
    link:https://github.com/ruby/debug/releases/tag/v1.10.0[Details].
    
    Milestone: patch

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ end
 
 group :tools do
   gem "amazing_print", "~> 1.6"
-  gem "debug", "~> 1.9"
+  gem "debug", "~> 1.10"
   gem "irb-kit", "~> 1.0"
   gem "repl_type_completor", "~> 0.1"
 end
--- spec/lib/gemsmith/builders/bundler_spec.rb
+++ spec/lib/gemsmith/builders/bundler_spec.rb
@@ -60,7 +60,7 @@ RSpec.describe Gemsmith::Builders::Bundler do
 
           group :tools do
             gem "amazing_print", "~> 1.6"
-            gem "debug", "~> 1.9"
+            gem "debug", "~> 1.10"
             gem "irb-kit", "~> 1.0"
             gem "repl_type_completor", "~> 0.1"
           end
