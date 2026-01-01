e963449556b63611a3e7a9a7b94b65fe4802ca8d
Brooke Kuhlmann <brooke@alchemists.io>
2026-01-01 11:49:25 -0700

    Updated to Git Lint 10.0.0
    link:https://alchemists.io/projects/git-lint/versions/10.0.0/[Details]. This only effects the development environment which is why this is a minor bump.
    
    Milestone: minor

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gemspec
 
 group :quality do
   gem "caliber", "~> 0.82"
-  # gem "git-lint", "~> 9.0"
+  gem "git-lint", "~> 10.0"
   gem "reek", "~> 6.5", require: false
   gem "simplecov", "~> 0.22", require: false
 end
--- spec/lib/gemsmith/builders/bundler_spec.rb
+++ spec/lib/gemsmith/builders/bundler_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe Gemsmith::Builders::Bundler do
 
           group :quality do
             gem "caliber", "~> 0.82"
-            gem "git-lint", "~> 9.0"
+            gem "git-lint", "~> 10.0"
             gem "reek", "~> 6.5", require: false
             gem "simplecov", "~> 0.22", require: false
           end
